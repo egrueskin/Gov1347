4127b8f3d9f98d48b359a4ac78b64a4ba0ab24db
Ellie Grueskin <elliegrueskin@dhcp-10-250-2-208.harvard.edu>
2022-11-06 13:12:44 -0500

    tr

---
+++ extra__.R
@@ -0,0 +1,14 @@
+test$pred_seats<-predict(lm_seats,test,interval='prediction')
+test$pred_vote<-predict(lm_vote,test,interval='prediction')
+tab<-data.frame(cbind(test[test$year==2022&test$party=='D',c(3,2,22)],
+                      test[test$year==2022&test$party=='R',c(3,2,23)]))%>%mutate(pred_vote_D=100-pred_vote,pred_st_R=435-pred_seats)
+tab<-data.frame(rbind(tab[,c(1:3,7)]%>% rename('pred_vote'='pred_vote_D'),tab[,c(4:6,8)]%>%
+                        rename('pred_seats'='pred_st_R','party'='party.1','year'='year.1')))
+
+
+mutate(pred_seats=ifelse(party=='R',))
+%>%
+  
+  tab<-data.frame(cbind(test[test$year==2022&test$party=='D',c(3,2,22:23)]%>%
+                          mutate(party_2='R',pred_seats_R=435-pred_seats,pred_vote_R=100-pred_vote)
+                        tab
