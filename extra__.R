ad23d28cab19e82de4a63efef41208885559a0ef
Ellie Grueskin <elliegrueskin@dhcp-10-250-2-208.harvard.edu>
2022-11-06 14:47:17 -0500

    img

--- extra__.R
+++ extra__.R
@@ -12,3 +12,18 @@ mutate(pred_seats=ifelse(party=='R',))
   tab<-data.frame(cbind(test[test$year==2022&test$party=='D',c(3,2,22:23)]%>%
                           mutate(party_2='R',pred_seats_R=435-pred_seats,pred_vote_R=100-pred_vote)
                         tab
+                        
+                        
+                        
+                        $$
+                          VoteShare=\beta_0+\beta_{1}SamePartyPres_i+\kappa_{im}SamePartyPres_i*MidtermYear_m+\\
+                        \gamma_iSamePartyPres_i*PresidentialApproval+\rho_iSamePartyPres_i*GDPgrowthpct+\\\beta_2*genericballotpct+
+                          \beta_3*lagvoteshare
+                        $$
+                          
+                          
+                          $$
+                          SeatShare=\beta_0+\beta_{1}SamePartyPres_i+\kappa_{im}SamePartyPres_i*MidtermYear_m+\\
+                        \gamma_iSamePartyPres_i*PresidentialApproval+\rho_iSamePartyPres_i*GDPgrowthpct+\\\beta_2*genericballotpct+
+                          \beta_3*lagseatshare
+                        $$
